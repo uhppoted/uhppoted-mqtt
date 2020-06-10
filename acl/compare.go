e0513b747f404e9c52294454d21b5e51df6f53af
Tony Seebregts <tony.seebregts@gmail.com>
2020-06-10 12:36:55 -0700

    Explicitly reported 'OK' for compare-acl when ACL and controllers match

--- acl/compare.go
+++ acl/compare.go
@@ -19,9 +19,8 @@ var templates = struct {
 	report string
 }{
 	report: `ACL DIFF REPORT {{ .DateTime }}
-
 {{range $id,$value := .Diffs}}
-  DEVICE {{ $id }}{{if $value.Unchanged}}
+  DEVICE {{ $id }}{{if or $value.Updated $value.Added $value.Deleted}}{{else}} OK{{end}}{{if $value.Updated}}
     Incorrect:  {{range $value.Updated}}{{.}}
                 {{end}}{{end}}{{if $value.Added}}
     Missing:    {{range $value.Added}}{{.}}
