01f83cd5863eb2cffe3a5b7ece434c2221ad9e1f
twystd <twystd@users.noreply.github.com>
2024-05-06 10:13:52 -0700

    Updated to use BindAddr accessor functions

--- commands/daemonize_linux.go
+++ commands/daemonize_linux.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"net/netip"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -192,7 +191,7 @@ func (cmd *Daemonize) execute() error {
 	fmt.Println()
 	fmt.Println("   The firewall may need additional rules to allow UDP broadcast e.g. for UFW:")
 	fmt.Println()
-	fmt.Printf("     > sudo ufw allow from %s to any port 60000 proto udp\n", netip.AddrPort(bind).Addr()) // FIXME BindAddr.Addr()
+	fmt.Printf("     > sudo ufw allow from %s to any port 60000 proto udp\n", bind.Addr())
 	fmt.Println()
 	fmt.Println("   Please replace the default RSA keys for event and system messages:")
 	fmt.Printf("     - %s\n", filepath.Join(filepath.Dir(cmd.config), "mqtt", "rsa", "encryption", "event.pub"))
